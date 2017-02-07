64d581dc3056c9a31e33eb065d1ff0655b9cd3ce
Justin Qassis <qassisju@msu.edu>
2017-02-07 18:15:55 -0500

    updated spacing

--- info.js
+++ info.js
@@ -5,19 +5,15 @@ var lastNameInput = document.getElementById('lastName')
 var describeYourself = document.getElementById('description')
 var emailInput = document.getElementById('email')
 var phoneInput = document.getElementById('phone')
-
 var addCode = function () {
   var firstName = firstNameInput.value
   var lastName = lastNameInput.value
   var describe = describeYourself.value
   var email = emailInput.value
   var phone = phoneInput.value
-
   preview.innerHTML = '<h1>' + 'Hi, my name is: ' + firstName + lastName + '</h1>' + '<p>' + describe + '</p>' + '<p>' + 'If you are interested in a date, please email me at ' + '<a href="mailto:' + email + '" target="_blank">' + email +'</a>' + 'or give me a call at' +  '<a href="tel:' + phone +  '"target="_blank">' +  phone + '</a>' + '</p>'
   actual.textContent =  '<h1>' + 'Hi, my name is: ' + firstName + lastName + '</h1>' + '<p>' + describe + '</p>' + '<p>' + 'If you are interested in a date, please email me at ' + '<a href="mailto:' + email + '" target="_blank">' + email +'</a>' + 'or give me a call at' +  '<a href="tel:' + phone +  '"target="_blank">' +  phone + '</a>' + '</p>'
-
 }
-
 firstNameInput.addEventListener('input', addCode)
 lastNameInput.addEventListener('input', addCode)
 describeYourself.addEventListener('input', addCode)
